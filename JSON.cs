e9d3aab4f4c9a1a0b8c7385fd8a8c39500e1c5cc
terrygardy <terry.gardner@live.de>
2016-07-28 00:06:48 +0200

    Version 1.0

---
+++ JSON.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WebHelpers
+{
+	public static class JSON
+	{
+		public static string serialiseObject<T>(T pv_objJSON)
+			where T : class
+		{
+			return JsonConvert.SerializeObject(pv_objJSON, Formatting.Indented, new JsonSerializerSettings() { DateFormatString = "dd.MM.yyyy" });
+		}
+
+		public static T deserialiseObject<T>(string pv_strJSON)
+			where T : class
+		{
+			return JsonConvert.DeserializeObject<T>(pv_strJSON, new JsonSerializerSettings() { DateFormatString = "dd.MM.yyyy" });
+		}
+	}
+}
